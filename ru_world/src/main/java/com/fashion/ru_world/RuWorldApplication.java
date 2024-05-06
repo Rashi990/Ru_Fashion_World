a8def5e1add4a977870fd3ea2f8ac099dfe9e04f
Rashini_112425 <rashinilakshika@Axiatadigitallabs.com>
2024-05-06 10:31:09 +0530

    spring boot initial commit

---
+++ ru_world/src/main/java/com/fashion/ru_world/RuWorldApplication.java
@@ -0,0 +1,13 @@
+package com.fashion.ru_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuWorldApplication.class, args);
+	}
+
+}
